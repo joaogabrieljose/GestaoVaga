a6824733a8276c8d3252bfad57967246c0565cc7
João José <joaogjose@outlook.com>
2025-02-09 22:08:37 +0000

    criação da entidade CandidateEntity

---
+++ src/main/java/com/example/gestaovaga/entity/candidate/CandidateEntity.java
@@ -0,0 +1,17 @@
+package com.example.gestaovaga.entity.candidate;
+
+
+import java.util.UUID;
+
+public class CandidateEntity {
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String description;
+    private String password;
+    private String curriculum;
+
+
+
+}
